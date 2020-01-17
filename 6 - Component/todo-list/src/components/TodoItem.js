2c6a7760c7f8ca8e279e79dd192599c82f80a229
thongvm316 <56250318+thongvm316@users.noreply.github.com>
2020-01-17 11:19:05 +0700

    20200117-2

---
+++ 6 - Component/todo-list/src/components/TodoItem.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+// tuong tu: const react = require('react');
+
+class TodoItem extends Component {
+  render() {
+      return (
+        <div class="TodoItem">
+            <p>Kito</p>
+        </div>
+      );
+  }
+}
+
+export default TodoItem;
