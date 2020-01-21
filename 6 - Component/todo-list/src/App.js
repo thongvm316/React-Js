f760b943bf6622d1b570ee73d8f0cd9f31c1284d
thongvm316 <56250318+thongvm316@users.noreply.github.com>
2020-01-21 17:13:10 +0700

    20200121

--- 6 - Component/todo-list/src/App.js
+++ 6 - Component/todo-list/src/App.js
@@ -1,36 +1,37 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import TodoItem from './components/TodoItem';
+  import React, { Component } from 'react';
+  // import logo from './logo.svg';
+  import './App.css';
+  import TodoItem from './components/TodoItem';
 
 
-class App extends Component {
-  constructor() {
-    super();
-    this.todoItems = [
-      " Di da bong", 
-      " Bong chuyen", 
-      " Bowling"
-    ];                  
-  }   
-  render() {
-    return (
-      <div className="App">
-        {
-          this.todoItems.map((item, index) => <TodoItem key={index} title={item}/>)
-        }
-      </div>
-    );  
+  class App extends Component {
+    constructor() {
+      super();
+      this.todoItems = [
+        { title: 'Da bong', isComplete: true },
+        { title: 'Cafe', isComplete: true },
+        { title: 'Film' }
+      ];                  
+    }; // luu data vao constructor Fn
+    render() {
+      return (
+        <div className="App">
+          {
+            this.todoItems.map((item, index) => 
+            <TodoItem key={index} item={item}/>)
+          }
+        </div>
+      );  
+    }
   }
-}
-////// cach viet truyen thong
-// function App() {
-//   return (
-//     <div className="App">
-//         <TodoItem /> 
-//         <TodoItem /> 
-//         <TodoItem /> 
-//     </div>
-//   );
+  ////// cach viet truyen thong
+  // function App() {
+  //   return (
+  //     <div className="App">
+  //         <TodoItem title="Da bong"/> 
+  //         <TodoItem title="Cafe"/> 
+  //         <TodoItem title="Film"/> 
+  //     </div>
+  //   );
 
-export default App;
+  export default App;
--- 6 - Component/todo-list/src/components/TodoItem.js
+++ 6 - Component/todo-list/src/components/TodoItem.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 // tuong tu: const react = require('react');
+import './Todoitem.css';
 
 class TodoItem extends Component {
   render() {
+      const { item } = this.props;
+      let className = 'TodoItem ';
+      if (item.isComplete) {
+        className += 'TodoItem-complete';
+      }
       return (
-        <div className="TodoItem">
-            <p>{this.props.title}</p>
+        <div className={className}>
+            <p>{this.props.item.title}</p>
         </div>
       );
   }
